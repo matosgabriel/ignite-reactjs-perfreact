73eae6ad07246570a2c91f8994aa394a91adeecf
Gabriel <gabriel.ifsal@gmail.com>
2022-09-08 10:08:16 -0300

    Adding coding splitting (lazy loading) to AddProductToWishList component

--- pages/components/ProductItem.tsx
+++ pages/components/ProductItem.tsx
@@ -1,4 +1,18 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import { AddProductToWishListProps } from './AddProductToWishList';
+
+import dynamic from 'next/dynamic';
+
+const AddProductToWishList = dynamic<AddProductToWishListProps>(
+  () => {
+    return import('./AddProductToWishList').then(
+      (mod) => mod.AddProductToWishList
+    );
+  },
+  {
+    loading: () => <span>Carregando...</span>,
+  }
+);
 
 interface ProductItemProps {
   product: { id: number; price: number; title: string; formattedPrice: string };
@@ -9,12 +23,20 @@ function ProductItemComponent({
   product,
   handleAddToWishList,
 }: ProductItemProps) {
+  const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.formattedPrice}</strong>
-      <button onClick={() => handleAddToWishList(product.id)}>
+      <button onClick={() => setIsAddingToWishList(true)}>
         Adicionar a lista de favoritos
       </button>
+      {isAddingToWishList && (
+        <AddProductToWishList
+          onAddToWishList={() => handleAddToWishList(product.id)}
+          onRequestClose={() => setIsAddingToWishList(false)}
+        />
+      )}
     </div>
   );
 }
