ea616ffbfbf607a8dab72c15927bf22791731396
Gabriel <gabriel.ifsal@gmail.com>
2022-09-08 10:21:24 -0300

    Adding virtualization on SearchResults component

--- pages/components/SearchResults.tsx
+++ pages/components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
+
 import { ProductItem } from './ProductItem';
 
 interface Product {
@@ -19,19 +20,29 @@ export function SearchResults({
   handleAddToWishList,
   totalPrice,
 }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          handleAddToWishList={handleAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            handleAddToWishList={handleAddToWishList}
-          />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
